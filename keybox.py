503a4c8462d2eb613f128ab54cccfc4ebc06751e
Jun QIN <jun.qin@amadeus.com>
2016-08-12 09:36:04 +0200

    use more meaningful 'title' (vs. 'content') instead of 'name'

--- keybox.py
+++ keybox.py
@@ -12,7 +12,7 @@ import argparse
 import Crypto.Cipher.AES
 
 class KeyBox(object):
-    MASTER_KEY_NAME = "<MASTER>"
+    MASTER_KEY_TITLE = "<MASTER>"
 
     def __init__(self, file):
         self.conn = sqlite3.connect(file)
@@ -22,40 +22,40 @@ class KeyBox(object):
         # The following line would use unicode string
         # self.conn.text_factory = lambda x: unicode(x, 'utf-8', 'ignore')
         self.cursor = self.conn.cursor()
-        self.cursor.execute('CREATE TABLE IF NOT EXISTS keys (name TEXT PRIMARY KEY, content BLOB)')
+        self.cursor.execute('CREATE TABLE IF NOT EXISTS keys (title TEXT PRIMARY KEY, content BLOB)')
         self.conn.commit()
 
     def list(self):
-        names = []
-        self.cursor.execute('SELECT name FROM keys ORDER BY name')
+        titles = []
+        self.cursor.execute('SELECT title FROM keys ORDER BY title')
         for row in self.cursor:
-            if row[0] != KeyBox.MASTER_KEY_NAME:
-                names.append( row[0] )
-        return names
+            if row[0] != KeyBox.MASTER_KEY_TITLE:
+                titles.append( row[0] )
+        return titles
 
-    def exists(self, name):
-        self.cursor.execute("SELECT content FROM keys WHERE name=?", (name,) )
+    def exists(self, title):
+        self.cursor.execute("SELECT content FROM keys WHERE title=?", (title,) )
         return self.cursor.fetchone() != None
 
-    def assertExist(self, name):
-        if not self.exists(name):
-            exitWithError("Error: '%s' not found, try to list all names or change to another name" % name)
+    def assertExist(self, title):
+        if not self.exists(title):
+            exitWithError("Error: '%s' not found, try to list all titles or change to another title" % title)
 
     def initOrVerifyMasterPassword(self):
-        if not self.exists(KeyBox.MASTER_KEY_NAME):
+        if not self.exists(KeyBox.MASTER_KEY_TITLE):
             password = inputPassword("Create a new master password: ")
             if password == inputPassword("Confirm the master password: "):
                 # the AES key of the master password, to encrypt key content
                 self.aesKey = hashlib.sha256(password).digest()
                 # the hash of the AES key, stored in db for master password verification
                 keyHash = hashlib.sha256(self.aesKey).hexdigest()
-                self.cursor.execute("INSERT INTO keys VALUES (?,?)", (KeyBox.MASTER_KEY_NAME, keyHash ) )
+                self.cursor.execute("INSERT INTO keys VALUES (?,?)", (KeyBox.MASTER_KEY_TITLE, keyHash ) )
                 self.conn.commit()
             else:
                 exitWithError("Error: password not match, please retry")
         else:
             # get the stored key hash
-            self.cursor.execute("SELECT content FROM keys WHERE name=?", (KeyBox.MASTER_KEY_NAME,) )
+            self.cursor.execute("SELECT content FROM keys WHERE title=?", (KeyBox.MASTER_KEY_TITLE,) )
             storedKeyHash = self.cursor.fetchone()[0]
             # input master password
             password = inputPassword("Master password: ")
@@ -64,28 +64,28 @@ class KeyBox(object):
             if hashlib.sha256(self.aesKey).hexdigest() != storedKeyHash:
                 exitWithError("Error: incorrect master password, please retry")
 
-    def view(self, name):
-        self.cursor.execute("SELECT content FROM keys WHERE name=?", (name,) )
+    def view(self, title):
+        self.cursor.execute("SELECT content FROM keys WHERE title=?", (title,) )
         encrypted = self.cursor.fetchone()[0]
         return decrypt(encrypted, self.aesKey)
 
-    def set(self, name, plain):
+    def set(self, title, plain):
         # for better print effect
         if plain[-1] != "\n": plain += "\n"
         encrypted = encrypt(plain, self.aesKey)
         self.cursor.execute("INSERT OR REPLACE INTO keys VALUES (?,?)",
-                            (name, sqlite3.Binary(encrypted) ) )
+                            (title, sqlite3.Binary(encrypted) ) )
         self.conn.commit()
 
-    def delete(self, name):
-        plain = self.view(name)
-        self.cursor.execute("delete FROM keys WHERE name=?", (name,) )
+    def delete(self, title):
+        plain = self.view(title)
+        self.cursor.execute("delete FROM keys WHERE title=?", (title,) )
         self.conn.commit()
         return plain
 
 
-def inputContent(name):
-    sys.stdout.write("Input content of '%s', enter an empty line to finish:\n" % name)
+def inputContent(title):
+    sys.stdout.write("Input content of '%s', enter an empty line to finish:\n" % title)
     lines = []
     while True:
         line = raw_input()
@@ -115,12 +115,12 @@ def readKeys(aFile):
     """
     Supported text file format is as follows:
 
-    key name1:
+    key title1:
     key content line 11
     key content line 12
     ...
 
-    key name2:
+    key title2:
     key content line 21
     key content line 22
     ...
@@ -128,31 +128,31 @@ def readKeys(aFile):
     """
     keys = []
     with open(aFile, 'r') as fd:
-        name = ''
+        title = ''
         contentLines = []
         for line in fd:
             line = line.strip()
-            if line.endswith(":"):  # name line
-                if name != '' and contentLines != []:
+            if line.endswith(":"):  # title line
+                if title != '' and contentLines != []:
                     # remove the empty lines at the end
                     while len(contentLines) > 0 and contentLines[-1] == "\n":
                         contentLines = contentLines[:-1]
                     # add to keys for return
-                    keys.append( (name, '\n'.join([aLine for aLine in contentLines])) )
-                # set next key name, and clear content
-                name = line[:-1]
+                    keys.append( (title, '\n'.join([aLine for aLine in contentLines])) )
+                # set next key title, and clear content
+                title = line[:-1]
                 contentLines = []
-            elif name != "":
+            elif title != "":
                 contentLines.append(line)
             else:
-                sys.stderr.write("Warn: line '%s' ignored: name missing\n" % line)
+                sys.stderr.write("Warn: line '%s' ignored: title missing\n" % line)
     # process the last key
-    if name != '' and contentLines != []:
+    if title != '' and contentLines != []:
         # remove the empty lines at the end
         while len(contentLines) > 0 and contentLines[-1] == "\n":
             contentLines = contentLines[:-1]
         # add to keys for return
-        keys.append( (name, '\n'.join([aLine for aLine in contentLines])) )
+        keys.append( (title, '\n'.join([aLine for aLine in contentLines])) )
 
     return keys
 
@@ -169,19 +169,19 @@ def main():
     subparsers = parser.add_subparsers(title="subcommands", dest="action",
             metavar='help|list|view|add|mod|del|import|export')
     helpParser = subparsers.add_parser("help", help="show this help message and exit")
-    subParser = subparsers.add_parser("list", help="list all key names")
-    subParser = subparsers.add_parser("view", help="view the content for the given key name")
-    subParser.add_argument("name", help="a key name")
-    subParser = subparsers.add_parser("add", help="add a new key name and content")
-    subParser.add_argument("name", help="a key name")
-    subParser = subparsers.add_parser("mod", help="modify the content for the given key name")
-    subParser.add_argument("name", help="a key name")
-    subParser = subparsers.add_parser("del", help="delete an existing key name and content")
-    subParser.add_argument("name", help="a key name")
-    subParser = subparsers.add_parser("import", help="import all key names and contents from a text file")
-    subParser.add_argument("file", help="a text file containing key names and contents to import")
-    subParser = subparsers.add_parser("export", help="export all key names and contents to stdout or a file")
-    subParser.add_argument("file", nargs='?', help="a text file to export the key names and contents")
+    subParser = subparsers.add_parser("list", help="list all key titles")
+    subParser = subparsers.add_parser("view", help="view the content for the given key title")
+    subParser.add_argument("title", help="a key title")
+    subParser = subparsers.add_parser("add", help="add a new key title and content")
+    subParser.add_argument("title", help="a key title")
+    subParser = subparsers.add_parser("mod", help="modify the content for the given key title")
+    subParser.add_argument("title", help="a key title")
+    subParser = subparsers.add_parser("del", help="delete an existing key title and content")
+    subParser.add_argument("title", help="a key title")
+    subParser = subparsers.add_parser("import", help="import all key titles and contents from a text file")
+    subParser.add_argument("file", help="a text file containing key titles and contents to import")
+    subParser = subparsers.add_parser("export", help="export all key titles and contents to stdout or a file")
+    subParser.add_argument("file", nargs='?', help="a text file to export the key titles and contents")
     args = parser.parse_args()
 
     if args.action == 'help':
@@ -190,20 +190,20 @@ def main():
 
     keybox = KeyBox(args.database)
     if args.action == 'list':
-        names = keybox.list()
-        if len(names) == 0:
+        titles = keybox.list()
+        if len(titles) == 0:
             sys.stdout.write("No item found\n")
         else:
-            for name in names:
-                print "- " + name
+            for title in titles:
+                print "- " + title
         sys.exit(0)
 
     # check errors before init or verify master password
     if args.action == 'add':
-        if keybox.exists(args.name):
-            exitWithError("Error: '%s' exists, try to view it or add with another name" % args.name)
+        if keybox.exists(args.title):
+            exitWithError("Error: '%s' exists, try to view it or add with another title" % args.title)
     if args.action in ['view', 'mod', 'del']:
-        keybox.assertExist(args.name)
+        keybox.assertExist(args.title)
     elif args.action == "import":
         if not os.path.exists(args.file):
             exitWithError("Error: file '%s' not found." % args.file)
@@ -217,35 +217,35 @@ def main():
 
     keybox.initOrVerifyMasterPassword()
     if args.action in ['view', 'mod', 'del']:
-         sys.stdout.write( "---\n%s:\n%s---\n" % (args.name, keybox.view(args.name) ) )
+         sys.stdout.write( "---\n%s:\n%s---\n" % (args.title, keybox.view(args.title) ) )
 
     if args.action == 'add':
-        plain = inputContent(args.name)
-        keybox.set(args.name, plain)
+        plain = inputContent(args.title)
+        keybox.set(args.title, plain)
     elif args.action == "view":
          sys.exit(0)
     elif args.action == "mod":
-        plain = inputContent(args.name)
-        keybox.set(args.name, plain)
+        plain = inputContent(args.title)
+        keybox.set(args.title, plain)
     elif args.action == "del":
-        confirm = raw_input("Confirm to delete key '%s' [yes/no]? " % args.name)
+        confirm = raw_input("Confirm to delete key '%s' [yes/no]? " % args.title)
         while confirm not in ['yes', 'no']:
-            confirm = raw_input("Confirm to delete key '%s' [yes/no]? " % args.name)
+            confirm = raw_input("Confirm to delete key '%s' [yes/no]? " % args.title)
         if confirm == 'yes':
-            keybox.delete(args.name)
+            keybox.delete(args.title)
             sys.stdout.write ("Deleted.\n")
     elif args.action == "import":
-        for name, content in readKeys(args.file):
-            if keybox.exists(name):
-                sys.stdout.write ("skipped %s: exists in database\n" % name)
+        for title, content in readKeys(args.file):
+            if keybox.exists(title):
+                sys.stdout.write ("skipped %s: exists in database\n" % title)
             else:
-                keybox.set(name, content)
-                sys.stdout.write ("imported %s\n" % name)
+                keybox.set(title, content)
+                sys.stdout.write ("imported %s\n" % title)
     elif args.action == "export":
         if fd == sys.stdout: fd.write("---\n")
-        for name in keybox.list():
-            fd.write("%s:\n" % name)
-            fd.write("%s" % keybox.view(name) )
+        for title in keybox.list():
+            fd.write("%s:\n" % title)
+            fd.write("%s" % keybox.view(title) )
             if fd == sys.stdout:
                 fd.write("---\n")
             else:
